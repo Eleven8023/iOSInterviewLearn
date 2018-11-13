46d68f56f8b6199bd1ce2f1567ce50f62dd9b348
李赐岩 <liciyan@zafh.com.cn>
2018-11-13 09:21:11 +0800

    Initial Commit

---
+++ iOSInterviewLearn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSInterviewLearn
+//
+//  Created by 李赐岩 on 2018/11/13.
+//  Copyright © 2018 李赐岩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSInterviewLearn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSInterviewLearn
+//
+//  Created by 李赐岩 on 2018/11/13.
+//  Copyright © 2018 李赐岩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
